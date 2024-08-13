c86cbfc9bfcc6173138efc359a7923b5cad0e09d
rohitbharti279 <rohitbharti279@gmail.com>
2024-08-13 17:24:59 +0530

    WIP: Update Liquity Features

--- src/Components/LiquityFeatures.tsx
+++ src/Components/LiquityFeatures.tsx
@@ -3,45 +3,45 @@ import React from 'react';
 const LiquityFeatures: React.FC = () => {
     return (
         <>
-            <div className="flex flex-col lg:flex-row gap-3 md:px-8 lg:gap-10 lg:px-16 xl:gap-14 xl:px-32 p-3 bg-slate-200">
-                <div className="bg-white shadow-lg rounded-lg p-6">
+            <div className="flex flex-col lg:flex-row gap-3 md:px-8 lg:gap-10 lg:px-16 xl:gap-14 xl:px-32 p-3 bg-slate-900">
+                <div className="rounded-lg p-6 hover:bg-slate-800 hover:shadow-lg hover:scale-105 transform transition-transform duration-300 ease-in-out">
                     <div className="flex flex-column place-items-center">
                         <img
                             src="https://via.placeholder.com/50"
                             alt="Logo"
                             className="h-10 w-10 mr-3"
                         />
-                        <h2 className="text-2xl font-bold">0% Interest Rate</h2>
+                        <h2 className="text-xl font-bold text-white">0% Interest Rate</h2>
                     </div>
-                    <p className="text-gray-700 mt-3">
+                    <p className="mt-3 text-slate-400">
                         Liquity charges a small, one-time fee to borrow LUSD instead of highly variable interest rates.
                     </p>
                 </div>
 
-                <div className="bg-white shadow-lg rounded-lg p-6">
+                <div className="rounded-lg p-6 hover:bg-slate-800 hover:shadow-lg hover:scale-105 transform transition-transform duration-300 ease-in-out">
                 <div className="flex flex-column place-items-center">
                     <img
                         src="https://via.placeholder.com/50"
                         alt="Logo"
                         className="h-10 w-10 mr-3"
                     />
-                    <h2 className="text-2xl font-bold">110% Collateral Ratio*</h2>
+                    <h2 className="text-xl font-bold text-white">110% Collateral Ratio*</h2>
                     </div>
-                    <p className="text-gray-700 mt-3">
+                    <p className="mt-3 text-slate-400">
                         Liquity's efficient liquidation mechanism allows users to get the most liquidity for their ETH.
                     </p>
                 </div>
 
-                <div className="bg-white shadow-lg rounded-lg p-6">
+                <div className="rounded-lg p-6 hover:bg-slate-800 hover:shadow-lg hover:scale-105 transform transition-transform duration-300 ease-in-out">
                 <div className="flex flex-column place-items-center">
                     <img
                         src="https://via.placeholder.com/50"
                         alt="Logo"
                         className="h-10 w-10 mr-3"
                     />
-                    <h2 className="text-2xl font-bold">Unstoppable Stablecoin</h2>
+                    <h2 className="text-xl font-bold text-white">Unstoppable Stablecoin</h2>
                     </div>
-                    <p className="text-gray-700 mt-3">
+                    <p className="mt-3 text-slate-400">
                         LUSD is an immutable, decentralized stablecoin with no centralized counterparty risk.
                     </p>
                 </div>
